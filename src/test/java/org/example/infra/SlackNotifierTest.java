76121396a59f42421e6aa5303e1c0987228b804b
cjm2357 <cjm2357@gmail.com>
2025-09-28 13:15:42 +0900

    add test

---
+++ src/test/java/org/example/infra/SlackNotifierTest.java
@@ -0,0 +1,14 @@
+package org.example.infra;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SlackNotifierTest {
+
+    @Test
+    @DisplayName("테스트 코드 작동여부 확인")
+    public void test1() {
+        System.out.println("ABC");
+    }
+}
